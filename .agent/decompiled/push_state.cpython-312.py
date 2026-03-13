7deb1177b13cd802c78ae343a14abfd1cd1c631d
TibetanMan <kerstinwunschdus39@gmail.com>
2026-03-14 03:03:26 +0800

    chore: ignore local artifacts and stop tracking generated files

--- .agent/decompiled/push_state.cpython-312.py
+++
@@ -1,6 +0,0 @@
-# decompyle3 version 3.9.3
-# Python bytecode version base 3.12.0 (3531)
-# Decompiled from: Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)]
-# Embedded file name: D:\Coding\Test\test-reflex\test_reflex\state\push_state.py
-# Compiled at: 2026-03-05 22:45:57
-# Size of source mod 2**32: 18994 bytes
--- .agent/decompiled/user_state.cpython-312.py
+++
@@ -1,6 +0,0 @@
-# decompyle3 version 3.9.3
-# Python bytecode version base 3.12.0 (3531)
-# Decompiled from: Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)]
-# Embedded file name: test_reflex/state/user_state.py
-# Compiled at: 2026-03-06 16:50:04
-# Size of source mod 2**32: 41457 bytes
